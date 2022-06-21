66dd10b7b99e948059c719f052c4df4391013614
suhas-2 <suhassogil.xworkz@gmail.com>
2022-06-21 10:00:59 +0530

    commiting Stationary file

---
+++ Stationary.java
@@ -0,0 +1,30 @@
+class Stationary{
+
+int id;
+String name;
+String location;
+int noOfWorkers;
+String openingTimings;
+String closingTimings;
+
+public static void main(String a[]){
+
+Stationary stationary = new Stationary();
+stationary.id = 145;	
+stationary.name = "Girija Stores";
+stationary.location = "Huskur";
+stationary.noOfWorkers =10;
+stationary.openingTimings = "7am";
+stationary.closingTimings = "9pm";
+
+
+	   System.out.println("id of stationary is " +stationary.id);
+	   System.out.println("name of stationary is " +stationary.name);
+	   System.out.println("adress of stationary is " +stationary.location);
+	   System.out.println("no of workers in stationary " +stationary.noOfWorkers);
+	   System.out.println("opening timings of stationary is " +stationary.openingTimings);
+	   System.out.println("closing timings of stationary is " +stationary.closingTimings);
+	   System.out.println("===========================");
+	   }
+	   }
+	   
